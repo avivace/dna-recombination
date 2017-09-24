050911229581b87e130b5b9e254e82cdf39ebc53
Antonio Vivace <puma95.95@gmail.com>
2017-09-24 09:12:16 +0200

    Gurobi use matrices in constraints/o.f. ?

--- script/basic.py
+++ script/basic.py
@@ -1,8 +1,9 @@
-## Imports and support classes
-
+## Imports, support classes and functions
+from __future__ import division
+from __future__ import print_function
 import random 
-import gurobipy
-import __future__
+from gurobipy import *
+
 
 class sparray(object):
 	def __init__(self, shape, default=0, dtype=int):
@@ -22,8 +23,10 @@ def reverse_complement(dna):
     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     return ''.join([complement[base] for base in dna[::-1]])
 
-## Artificially generate a basic instance of the problem (no overlap)
+## Artificially generate a basic instance of the problem
+## TODO: Add overlapping regions (pointers)
 
+print("Generated instance:")
 mic = ""
 mac = ""
 
@@ -37,26 +40,24 @@ while (i<micl):
 	mic = mic + numNotation[c]
 	i+=1
 
-
-
 MDSn = random.randint(2,10)
-print(MDSn, "MDS")
-
+MDSin = 0
 m = 0
 while (m < MDSn):
 	startOffset = random.randint(0, int(micl/MDSn))
 	length = random.randint(10,50)
-	print("MDS",m,"starts at",int(startOffset+(micl/MDSn)*m), "and ends at", int(startOffset+(micl/MDSn)*m)+length)
 	inverse = random.randint(0,4)
 	if (inverse == 4):
-		print("inversing MDS",m)
 		mac = mac + reverse_complement(mic[int(startOffset+(micl/MDSn)*m):int(startOffset+(micl/MDSn)*m)+length])
+		MDSin = MDSin + 1
 	else:
 		mac = mac + mic[int(startOffset+(micl/MDSn)*m):int(startOffset+(micl/MDSn)*m)+length]
+	print("MDS",m,"",int(startOffset+(micl/MDSn)*m), ":", int(startOffset+(micl/MDSn)*m)+length)
 	m+=1
 
-print("MIC  ", mic)
-print("MAC  ", mac)
+print("MIC:",micl,"MAC:", len(mac),"-", MDSn, "MDSs,", MDSin, "inverted")
+
+mac_i = reverse_complement(mac)
 
 ## Preprocess Phase
 
@@ -64,18 +65,30 @@ print("MAC  ", mac)
 # Eq(i,j,k,l) = 1 iff MIC[i:j]=MAC[h,l]
 
 Eq = sparray((len(mic),len(mic),len(mac),len(mac)))
+cwc = sparray((len(mic),len(mic),len(mac),len(mac)))
 
 matches = 0
-
-print("Populating Eq and cwc")
+i_matches = 0
+print("\nPopulating Eq and cwc")
 for i in range(0, len(mic)-1):
 	for j in range (i, len(mic)-1):
 		for a in range(0, len(mac)-(j-i)):
 			#print(i,j,a,a+(j-i))
 			if (mic[i:j] == mac[a:a+(j-i)]):
+				Eq[i,j,a,a+(j-i)] = 1
 				matches = matches+1
-				if (j-i) > 20:
-					print("Match with length ",j-i)
-			elif (mic[i:j] == reverse_complement(mac[a:a+(j-i)])):
-				if (j-i) > 20:
-					print("Inverse match with length ",j-i)
+			if (mic[i:j] == mac_i[a:a+(j-i)]):
+				cwc[i,j,a,a+(j-i)] = 1
+				i_matches = i_matches+1
+
+print("Matches: Eq:", matches,"cwc:", i_matches)
+
+## ILP formulation
+
+m = Model("dnar")
+
+MDS_Mac_Start = m.addVars(vtype=GRB.BINARY, name="MDS_Mac_Start")
+
+t = 0
+
+for t in range(0..)
